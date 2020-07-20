82b91aa1d65c1d143ba0d03ea8abea2d1665b265
Ces <cesmejia@gruposeara.com>
2020-07-20 12:59:54 -0500

    remove for loop and comment previous code

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
+// var rpio = require('rpio');
+// rpio.open(37, rpio.OUTPUT, rpio.LOW);
+ 
+// for (var i = 0; i < 5; i++) {
+//         /* On for 2 second */
+//         rpio.write(37, rpio.HIGH); 
+//         rpio.sleep(2);
+ 
+//         /* Off for 1 second */
+//         rpio.write(37, rpio.LOW);
+//         rpio.sleep(1);
+// }
+
+
 var rpio = require('rpio');
 rpio.open(37, rpio.OUTPUT, rpio.LOW);
  
-for (var i = 0; i < 5; i++) {
-        /* On for 2 second */
-        rpio.write(37, rpio.HIGH); 
-        rpio.sleep(2);
- 
-        /* Off for 1 second */
-        rpio.write(37, rpio.LOW);
-        rpio.sleep(1);
-}
+/* On for 2 second */
+rpio.write(37, rpio.HIGH); 
+rpio.sleep(2);
+
+/* Off for 1 second */
+rpio.write(37, rpio.LOW);
+rpio.sleep(1);
